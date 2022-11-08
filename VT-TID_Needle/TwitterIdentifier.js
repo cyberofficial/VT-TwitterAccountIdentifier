f20193c737798567aec346edeb2b7231a222264c
cyber <joeykoen@gmail.com>
2022-11-08 17:40:43 -0500

    Code redux
    Better handling of site
    moved debugger to different script

--- VT-TID_Needle/TwitterIdentifier.js
+++ VT-TID_Needle/TwitterIdentifier.js
@@ -1,6 +1,7 @@
 if(typeof init === 'undefined'){
 	const init = function(){
 
+var debugmode = false;
 
 console.log('vt needle loaded');
 
@@ -10,6 +11,8 @@ icon.innerHTML =
 var icon2 =
 	`<svg  style="color: lightgreen" viewBox="0 0 24 24" aria-label="vtchecked replaceme" role="img" class="r-13v1u17 r-4qtqp9 r-yyyyoo r-1xvli5t r-9cviqr r-f9ja8p r-og9te1 r-bnwqim r-1plcrui r-lrvibr"><g><path d="M22.25 12c0-1.43-.88-2.67-2.19-3.34.46-1.39.2-2.9-.81-3.91s-2.52-1.27-3.91-.81c-.66-1.31-1.91-2.19-3.34-2.19s-2.67.88-3.33 2.19c-1.4-.46-2.91-.2-3.92.81s-1.26 2.52-.8 3.91c-1.31.67-2.2 1.91-2.2 3.34s.89 2.67 2.2 3.34c-.46 1.39-.21 2.9.8 3.91s2.52 1.26 3.91.81c.67 1.31 1.91 2.19 3.34 2.19s2.68-.88 3.34-2.19c1.39.45 2.9.2 3.91-.81s1.27-2.52.81-3.91c1.31-.67 2.19-1.91 2.19-3.34zm-11.71 4.2L6.8 12.46l1.41-1.42 2.26 2.26 4.8-5.23 1.47 1.36-6.2 6.77z"></path></g></svg>`;
 // returns true if the anchor is a profile link by checking its attributes.
+
+
 
 function isProfileLink(a)
 {
@@ -24,7 +27,6 @@ function isProfileLink(a)
 	}
 	return true;
 }
-
 
 // returns true if the user screen name contained in the anchor is in the database.
 function isvtcheckedLink(a)
@@ -89,15 +91,31 @@ function isvtcheckedDiv(div)
 }
 
 
+
 window.setInterval(function () {
+	
 	var docurl = document.URL;
 	docurl = docurl.split("/");
 	docurl_or = docurl[3];
 	docurl = docurl[3].toLowerCase();
 
+	var errormessage = `
+	-------------------------------------------------
+	There was was an error
+	You can use this info below and create an issue over at: https://github.com/cyberofficial/VT-TwitterAccountIdentifier/issues
+
+	Error Message: 
+
+	`;
+
 	try {
-		console.log(document.querySelectorAll(`div=tabindex="-1"`))
-	} catch {}
+		for (const span of document.querySelectorAll('div[role="link"] div[dir="ltr"] > span')) {
+			if (!span.textContent.startsWith('@')) continue;
+			if (!span.innerHTML.includes('aria-label="vtchecked retweetcard"')) {
+				span.innerHTML = span.innerHTML + icon2.replace("replaceme", "retweetcard");
+			}
+		}
+	} catch (ex) {}
 
 
 	// profile links
@@ -112,19 +130,86 @@ window.setInterval(function () {
 					decoratevtchecked(a);
 				}
 			}
-	} catch {}
+	} catch (ex) {
+		if (debugmode){
+		console.warn(errormessage);
+		console.warn("Error Section: Profile Links\n");
+		console.warn(ex);
+		}
+
+	}
+
+	// private messages
+	try
+	{
+		for (const div of document.querySelectorAll('div[data-testid="conversation"]'))
+		{
+			var conv_div_4 = div;
+			var conv_div_4_1 = conv_div_4.childNodes[0];
+			var conv_div_4_1_2 = conv_div_4_1.childNodes[1];
+			if (isvtcheckedDiv(conv_div_4_1_2))
+			{
+				decoratevtchecked(conv_div_4_1_2);
+			}
+		}
+	}
+	catch (ex)
+	{
+		if (debugmode){
+		console.warn(errormessage);
+		console.warn("Error Section: Private Messages \n");
+		console.warn(ex);
+		}
+	}
+
+	// User Cells
+	try
+	{
+		for (const div of document.querySelectorAll('div[data-testid="UserCell"], div[data-testid="TypeaheadUser"], span[data-testid="socialContext"]'))
+		{
+			if (isvtcheckedDiv(div))
+			{
+				decoratevtchecked(div);
+			}
+		}
+	} catch (ex)
+	{
+		if (debugmode){
+		console.warn(errormessage);
+		console.warn("Error Section: User Cells \n");
+		console.warn(ex);
+		}
+	}
 
 	// Main Twitter Bio
 	try {
-		var mainprofile_innerhtml = document.querySelector("main").querySelector('div[aria-label="Home timeline"]').lastElementChild.firstElementChild.querySelector('div').firstElementChild.lastElementChild.querySelector('div[data-testid="UserName"] > div > div').lastElementChild.querySelector('span').innerHTML;
-		var handle_or = mainprofile_innerhtml.split("@")[1].split(" ")[0];
-		var handle = mainprofile_innerhtml.split("@")[1].split(" ")[0].toLowerCase();
+		try {
+			var mainprofile_innerhtml = document.querySelector("main").querySelector('div[aria-label="Home timeline"]').lastElementChild.firstElementChild.querySelector('div').firstElementChild.lastElementChild.querySelector('div[data-testid="UserName"] > div > div').lastElementChild.querySelector('span').innerHTML;
+		} catch (ex) {
+			if (debugmode){
+			console.warn(errormessage);
+			console.warn("Error Section: Main Profile Inner HTML Error \n");
+			console.warn(ex);
+			}
+		}
+		try {
+			var handle_or = mainprofile_innerhtml.split("@")[1].split(" ")[0];
+			var handle = mainprofile_innerhtml.split("@")[1].split(" ")[0].toLowerCase();
+		} catch (ex) {
+			if (debugmode){
+			console.warn(errormessage);
+			console.warn("Error Section: Main Profile Splitting Info\n");
+			console.warn(ex);
+			}
+		}
+
 
 		if(handle != docurl){
 			//console.log("Normal handle: " + docurl_or)
 			document.querySelector("main").querySelector('div[aria-label="Home timeline"]').lastElementChild.firstElementChild.querySelector('div').firstElementChild.lastElementChild.querySelector('div[data-testid="UserName"] > div > div').lastElementChild.querySelector('span').innerHTML = mainprofile_innerhtml.replace(handle_or, docurl_or);
 		}
 
+		// Main Twitter Bio Section 2
 		try {
 			if(!mainprofile_innerhtml.includes(`aria-label="vtchecked main profile"`)){
 				//console.log("Icon was NOT found. Adding it now.");
@@ -148,8 +233,20 @@ window.setInterval(function () {
 			}
 
 		//console.log("Found Profile Handle: " + handle);
-		} catch {}
-	} catch {}
+		} catch (ex) {
+			if (debugmode){
+			console.warn(errormessage);
+			console.warn("Error Section: Main Twitter Bio Section 2\n");
+			console.warn(ex);
+			}
+		}
+	} catch (ex) {
+		if (debugmode){
+		console.warn(errormessage);
+		console.warn("Error Section: Main Twitter Bio Section  1\n");
+		console.warn(ex);
+		}
+	}
 
 	// Check all @s
 	try {
@@ -159,6 +256,7 @@ window.setInterval(function () {
 			try {
 			
 			var b = a.querySelector('span').innerHTML.replace("@","");
+			
 			//console.log(b.toLowerCase());
 			if (vtchecked_users.includes(b.toLowerCase())) {
 				//console.log("Member found in database.");
@@ -167,18 +265,20 @@ window.setInterval(function () {
 				}
 			}
 			
-			} catch {}
-
-			
-			//var s = span.textContent.substring(1).toLowerCase();
-			//if (vtchecked_users.includes(a)) {
-			//	console.log(`Found Verified User: ` + a);
-			//}
+			} catch (ex) {
+				break;
+			}
 		}
-	} catch (ex) {}
+	} catch (ex) {
+		if (debugmode){
+		console.warn(errormessage);
+		console.warn("Error Section: Checking for @s Section Whole\n");
+		console.warn(ex);
+		}
+	}
 	
 
-}, 1000)
+}, 300)
 
 	}
 init();
