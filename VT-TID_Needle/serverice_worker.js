0f366a7f9b91aa0469af78b4b380b38d0105b994
cyber <joeykoen@gmail.com>
2022-11-08 18:58:01 -0500

    Fixed some issues and better formatting
    Fixed some issues and better formatting

--- VT-TID_Needle/serverice_worker.js
+++ VT-TID_Needle/serverice_worker.js
@@ -13,11 +13,5 @@ try {
     console.log(e);
 }
 
-//chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-//    if (request.type == "click_event") {
-//      console.log("Save Button was pressed!");
-//      // Call the callback passed to chrome.action.onClicked
-//    }
-//});
 
 console.log("Service Worker Working")
